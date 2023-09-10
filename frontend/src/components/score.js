ba740c4b789cc1471af6cfe019ffd314e4b4ea87
Sahith Reddy <sahithreddy.challarapu@gmail.com>
2023-09-10 12:02:12 +0530

    score updated

--- frontend/src/components/score.js
+++ frontend/src/components/score.js
@@ -20,25 +20,66 @@ import { Line, Bar, Pie,Doughnut } from "react-chartjs-2";
 // };
 const scores=localStorage.getItem('scores')
 console.log(scores)
-const response = 
-{
-    'eyecontact': 20,
-    'face_matched': true,
-    'jd_score': 30,
-    'leetcode': {
-      'easy': 1 / 12 ,
-      'medium': 14 / 240 ,
-      'hard': 16 / 100 ,
-      'active days': 45 ,
+// const response1 = 
+// {
+//     'eyecontact': 20,
+//     'face_matched': true,
+//     'jd_score': 30,
+//     'leetcode': {
+//       'easy': 1 / 12 ,
+//       'medium': 14 / 240 ,
+//       'hard': 16 / 100 ,
+//       'active days': 45 ,
+//     },
+//     'github': {
+//       'username': 'xyz',
+//       'last commit': 12,
+//     },
+//   }
+  const response ={
+    "_id": {
+      "$oid": "64fd59a394e2ec9baba16696"
     },
-    'github': {
-      'username': 'xyz',
-      'last commit': 12,
+    "job_d": "1",
+    "results": true,
+    "eye_contact_time": 49,
+    "Jd_score": 43.17,
+    "leetcode": {
+      "problem_sloved": [
+        "Easy112/714Beats92.7",
+        "Medium24/1510Beats61.0",
+        "Hard0/627Not enough data"
+      ],
+      "active days": "41"
     },
+    "github": {
+      "username": "Pavan Kumar Settem",
+      "contributions": "150"
+    }
   }
+
+  const res = response.leetcode.problem_sloved;
+
+    const extractedValues = res.map((str) => {
+    const match = str.match(/(\w+\d+\/\d+)/);
+    return match ? match[0] : null;
+    });
+
+    console.log(extractedValues);
+
+
+    let easyValue = (extractedValues[0].slice(4).split("/"))
+    let easy = parseInt(easyValue[0])/parseInt(easyValue[1])
+
+    let mediumValue = (extractedValues[1].slice(6).split("/"))
+    let medium = parseInt(mediumValue[0])/parseInt(mediumValue[1])
+
+    let hardValue = (extractedValues[2].slice(4).split("/"))
+    let hard = parseInt(hardValue[0])/parseInt(hardValue[1])
+
 let face_score = 5;
-if(response.face_matched){let face_score = 5}else{let face_score=0}
-const overallScore = (response.eyecontact/60 * 20)+(face_score)+(response.jd_score/100 * 50)+(response.github["last commit"]/20 *5)+(response.leetcode["active days"]/100 *20)
+if(true){let face_score = 5}else{let face_score=0}
+const overallScore = (response.eye_contact_time/60 * 20)+(face_score)+(response.Jd_score/100 * 50)+(response.github.contributions/500 *5)+(response.leetcode["active days"]/100 *20)
 console.log(overallScore)
   const responseDataEasy = {
     labels: ["Easy", "Total Easy"],
@@ -50,7 +91,7 @@ console.log(overallScore)
         borderWidth: 1,
         hoverBackgroundColor: ['rgba(75, 192, 192, 0.8)', 'rgba(255, 206, 86, 0.8)'],
         hoverBorderColor: "rgba(0,0,0,0)",
-        data: [response.leetcode.easy*100, 100-response.leetcode.easy],
+        data: [easy*100, 100-easy],
       },
     ],
   };
@@ -64,7 +105,7 @@ console.log(overallScore)
         borderWidth: 1,
         hoverBackgroundColor: ['rgba(75, 192, 192, 0.8)', 'rgba(255, 206, 86, 0.8)'],
         hoverBorderColor: "rgba(0,0,0,0)",
-        data: [response.leetcode.hard*100, 100-response.leetcode.hard],
+        data: [hard*100, 100-hard],
       },
     ],
   };
@@ -78,7 +119,7 @@ console.log(overallScore)
         borderWidth: 1,
         hoverBackgroundColor: ['rgba(75, 192, 192, 0.8)', 'rgba(255, 206, 86, 0.8)'],
         hoverBorderColor: "rgba(0,0,0,0)",
-        data: [response.leetcode.medium*100, 100-response.leetcode.medium],
+        data: [medium*100, 100-medium],
       },
     ],
   };
@@ -92,9 +133,42 @@ console.log(overallScore)
         borderWidth: 1,
         hoverBackgroundColor: ['rgba(75, 192, 192, 0.8)', 'rgba(255, 99, 132, 0.8)', 'rgba(255, 206, 86, 0.8)', 'rgba(54, 162, 235, 0.8)',"rgba(31, 7, 246, 1.41)"],
         hoverBorderColor: "rgba(0,0,0,0)",
-        data: [response.eyecontact/60 * 20, face_score, response.jd_score/100 * 50, response.github["last commit"]/20 *5, response.leetcode["active days"]/100 *20],
+        data: [response.eye_contact_time/60 * 20, face_score, response.Jd_score/100 * 50, response.github.contributions/500 *5, response.leetcode["active days"]/100 *20],
       },
     ],
+  };
+  const optionsMedium = {
+    plugins: {
+      title: {
+        display: true,
+        text: "Medium Level", // Set the title text here
+        font: {
+          size: 24,
+        },
+      },
+    },
+  };
+  const optionsEasy = {
+    plugins: {
+      title: {
+        display: true,
+        text: "Easy Level", // Set the title text here
+        font: {
+          size: 24,
+        },
+      },
+    },
+  };
+  const optionsHard = {
+    plugins: {
+      title: {
+        display: true,
+        text: "Hard Level", // Set the title text here
+        font: {
+          size: 24,
+        },
+      },
+    },
   };
 
 const Score = () => {
@@ -128,22 +202,16 @@ const Score = () => {
                 
                 <Pie
                     data={responseDataEasy}
-                    // options={{
-                    // scales: {
-                    //     y: {
-                    //     beginAtZero: true,
-                    //     },
-                    // },
-                    // }}
+                    options={optionsEasy} 
                 />
                 
             </div>
             <div className="chart-container ml-auto">
-            <Pie data={responseDataMedium}/>
+            <Pie data={responseDataMedium} options={optionsMedium} />
                 
             </div>
             <div className="chart-container ml-auto">
-            <Pie data={responseDataHard}/>
+            <Pie data={responseDataHard} options={optionsHard} />
                 
             </div>
         </div>
